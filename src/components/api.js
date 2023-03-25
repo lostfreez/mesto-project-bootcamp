68ef6eab0a30b8ad2f3534c4306c7eb6f27f641c
Anton Troickiy <lostfreez@yandex.ru>
2023-03-26 01:26:22 +0300

    refactor: константы api.js  добавлены в объект конфига

--- src/components/api.js
+++ src/components/api.js
@@ -1,75 +1,82 @@
-// Создаем константы
-const BASE_URL = "https://nomoreparties.co/v1/wbf-cohort-6/";
-const HEADERS = {
-authorization: "54974c2d-ab71-4b56-9932-c842ca70e522",
-"Content-Type": "application/json",
-};
-
-// Функции с использованием констант
+//конфиг с адресом и токеном
+const config = {
+  baseUrl: "https://nomoreparties.co/v1/wbf-cohort-6/",
+  headers: {
+    authorization: "54974c2d-ab71-4b56-9932-c842ca70e522",
+    "Content-Type": "application/json",
+  }
+}
+//функции запросов на сервер
 export function dislikeRequest(id) {
-return fetch(`${BASE_URL}cards/likes/${id}`, {
-method: "DELETE",
-headers: HEADERS,
-}).then(checkResponse);
-}
-export function likeRequest(id) {
-return fetch(`${BASE_URL}cards/likes/${id}`, {
-method: "PUT",
-headers: HEADERS,
-}).then(checkResponse);
-}
-export function deleteRequest(cardDelete) {
-return fetch(`${BASE_URL}cards/${cardDelete}`, {
-method: "DELETE",
-headers: HEADERS,
-}).then(checkResponse);
-}
-export function createCardRequest(inputNamePlace, inputUrlPlace) {
-return fetch(`${BASE_URL}cards`, {
-method: "POST",
-headers: HEADERS,
-body: JSON.stringify({
-name: inputNamePlace.value,
-link: inputUrlPlace.value,
-}),
-}).then(checkResponse);
-}
-export function saveProfileRequest(inputName, inputJob) {
-return fetch(`${BASE_URL}users/me`, {
-method: "PATCH",
-headers: HEADERS,
-body: JSON.stringify({
-name: inputName.value,
-about: inputJob.value,
-}),
-}).then(checkResponse);
-}
-export function updateAvatarRequest(inputAvatar) {
-return fetch(`${BASE_URL}users/me/avatar`, {
-method: "PATCH",
-headers: HEADERS,
-body: JSON.stringify({
-avatar: inputAvatar.value,
-}),
-}).then(checkResponse);
-}
-export function getCardsRequest() {
-return fetch(`${BASE_URL}cards`, {
-headers: HEADERS,
-}).then(checkResponse);
-}
-export function getUserRequest() {
-return fetch(`${BASE_URL}users/me`, {
-headers: HEADERS,
-}).then(checkResponse);
+  return fetch(`${config.baseUrl}cards/likes/${id}`, {
+    method: "DELETE",
+    headers: config.headers,
+  }).then(checkResponse);
 }
 
-// Функция обработчик ответа от сервера
-function checkResponse(response) {
-if (!response.ok) {
-return response.json().then((errorData) => {
-console.error("Ошибка HTTP: " + response.status, errorData);
-});
+export function likeRequest(id) {
+  return fetch(`${config.baseUrl}cards/likes/${id}`, {
+    method: "PUT",
+    headers: config.headers,
+  }).then(checkResponse);
 }
-return response.json();
+
+export function deleteRequest(cardDelete) {
+  return fetch(`${config.baseUrl}cards/${cardDelete}`, {
+    method: "DELETE",
+    headers: config.headers,
+  }).then(checkResponse);
+}
+
+export function createCardRequest(inputNamePlace, inputUrlPlace) {
+  return fetch(`${config.baseUrl}cards`, {
+    method: "POST",
+    headers: config.headers,
+    body: JSON.stringify({
+      name: inputNamePlace.value,
+      link: inputUrlPlace.value,
+    }),
+  }).then(checkResponse);
+}
+
+export function saveProfileRequest(inputName, inputJob) {
+  return fetch(`${config.baseUrl}users/me`, {
+    method: "PATCH",
+    headers: config.headers,
+    body: JSON.stringify({
+      name: inputName.value,
+      about: inputJob.value,
+    }),
+  }).then(checkResponse);
+}
+
+export function updateAvatarRequest(inputAvatar) {
+  return fetch(`${config.baseUrl}users/me/avatar`, {
+    method: "PATCH",
+    headers: config.headers,
+    body: JSON.stringify({
+      avatar: inputAvatar.value,
+    }),
+  }).then(checkResponse);
+}
+
+export function getCardsRequest() {
+  return fetch(`${config.baseUrl}cards`, {
+    headers: config.headers,
+  }).then(checkResponse);
+}
+
+export function getUserRequest() {
+  return fetch(`${config.baseUrl}users/me`, {
+    headers: config.headers,
+  }).then(checkResponse);
+}
+//универсальная функция обработчик запросов от сервера
+function checkResponse(response) {
+  if (!response.ok) {
+    return response.json().then((errorData) => {
+      console.error("Ошибка HTTP: " + response.status, errorData);
+    });
+  }
+  return response.json();
 }

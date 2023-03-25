ee33a80d71e17ba11b65c68be49d451f0f85f019
Anton Troickiy <lostfreez@yandex.ru>
2023-03-26 01:19:50 +0300

    refactor: ссылки и заголовки заменены на константы

--- src/components/api.js
+++ src/components/api.js
@@ -1,87 +1,75 @@
-//функции отправки запросов на сервер
+// Создаем константы
+const BASE_URL = "https://nomoreparties.co/v1/wbf-cohort-6/";
+const HEADERS = {
+authorization: "54974c2d-ab71-4b56-9932-c842ca70e522",
+"Content-Type": "application/json",
+};
+
+// Функции с использованием констант
 export function dislikeRequest(id) {
-  return fetch(`https://nomoreparties.co/v1/wbf-cohort-6/cards/likes/${id}`, {
-    method: "DELETE",
-    headers: {
-      authorization: "54974c2d-ab71-4b56-9932-c842ca70e522",
-    },
-  }).then(checkResponse);
+return fetch(`${BASE_URL}cards/likes/${id}`, {
+method: "DELETE",
+headers: HEADERS,
+}).then(checkResponse);
 }
 export function likeRequest(id) {
-  return fetch(`https://nomoreparties.co/v1/wbf-cohort-6/cards/likes/${id}`, {
-    method: "put",
-    headers: {
-      authorization: "54974c2d-ab71-4b56-9932-c842ca70e522",
-    },
-  }).then(checkResponse);
+return fetch(`${BASE_URL}cards/likes/${id}`, {
+method: "PUT",
+headers: HEADERS,
+}).then(checkResponse);
 }
 export function deleteRequest(cardDelete) {
-  return fetch(`https://nomoreparties.co/v1/wbf-cohort-6/cards/${cardDelete}`, {
-    method: "DELETE",
-    headers: {
-      authorization: "54974c2d-ab71-4b56-9932-c842ca70e522",
-    },
-  }).then(checkResponse);
+return fetch(`${BASE_URL}cards/${cardDelete}`, {
+method: "DELETE",
+headers: HEADERS,
+}).then(checkResponse);
 }
 export function createCardRequest(inputNamePlace, inputUrlPlace) {
-  return fetch("https://nomoreparties.co/v1/wbf-cohort-6/cards", {
-    method: "POST",
-    headers: {
-      authorization: "54974c2d-ab71-4b56-9932-c842ca70e522",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      name: inputNamePlace.value,
-      link: inputUrlPlace.value,
-    }),
-  }).then(checkResponse);
+return fetch(`${BASE_URL}cards`, {
+method: "POST",
+headers: HEADERS,
+body: JSON.stringify({
+name: inputNamePlace.value,
+link: inputUrlPlace.value,
+}),
+}).then(checkResponse);
 }
 export function saveProfileRequest(inputName, inputJob) {
-  return fetch("https://nomoreparties.co/v1/wbf-cohort-6/users/me", {
-    method: "PATCH",
-    headers: {
-      authorization: "54974c2d-ab71-4b56-9932-c842ca70e522",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      name: inputName.value,
-      about: inputJob.value,
-    }),
-  }).then(checkResponse);
+return fetch(`${BASE_URL}users/me`, {
+method: "PATCH",
+headers: HEADERS,
+body: JSON.stringify({
+name: inputName.value,
+about: inputJob.value,
+}),
+}).then(checkResponse);
 }
 export function updateAvatarRequest(inputAvatar) {
-  return fetch("https://nomoreparties.co/v1/wbf-cohort-6/users/me/avatar", {
-    method: "PATCH",
-    headers: {
-      authorization: "54974c2d-ab71-4b56-9932-c842ca70e522",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      avatar: inputAvatar.value,
-    }),
-  }).then(checkResponse);
+return fetch(`${BASE_URL}users/me/avatar`, {
+method: "PATCH",
+headers: HEADERS,
+body: JSON.stringify({
+avatar: inputAvatar.value,
+}),
+}).then(checkResponse);
 }
 export function getCardsRequest() {
-  return fetch("https://nomoreparties.co/v1/wbf-cohort-6/cards", {
-    headers: {
-      authorization: "54974c2d-ab71-4b56-9932-c842ca70e522",
-    },
-  }).then(checkResponse);
+return fetch(`${BASE_URL}cards`, {
+headers: HEADERS,
+}).then(checkResponse);
 }
 export function getUserRequest() {
-  return fetch("https://nomoreparties.co/v1/wbf-cohort-6/users/me", {
-    headers: {
-      authorization: "54974c2d-ab71-4b56-9932-c842ca70e522",
-    },
-  }).then(checkResponse);
+return fetch(`${BASE_URL}users/me`, {
+headers: HEADERS,
+}).then(checkResponse);
 }
 
-//Функция обработчик ответа от сервера
+// Функция обработчик ответа от сервера
 function checkResponse(response) {
-  if (!response.ok) {
-    return response.json().then((errorData) => {
-      console.error("Ошибка HTTP: " + response.status, errorData);
-    });
-  }
-  return response.json();
+if (!response.ok) {
+return response.json().then((errorData) => {
+console.error("Ошибка HTTP: " + response.status, errorData);
+});
+}
+return response.json();
 }
